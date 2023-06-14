d1d9d29745374a22fb5654185360a6472695c099
Lucas-SpiridonasNoack4321 <Lucas-Spiridonas.Noack@ottogroup.com>
2023-06-14 16:23:03 +0200

    feat/dark_mode

--- packages/client/src/utils/themes/index.ts
+++ packages/client/src/utils/themes/index.ts
@@ -94,6 +94,50 @@ const defaultTheme = () => {
   })
 }
 
+const darkTheme = () => {
+  return createTheme({
+    palette: {
+      type: 'dark',
+      background: {
+        default: '#F7F8F8',
+      },
+      primary: {
+        main: '#EC6559',
+      },
+      secondary: {
+        main: '#566C80',
+      },
+      chart: [
+        // primary
+        { main: '#E62314' },
+        { main: '#6D797F' },
+        { main: '#566C80' },
+        { main: '#66A8C3' },
+        { main: '#F7BDB8' },
+        { main: '#D3C661' },
+        { main: '#B01B10' },
+        { main: '#70ADA3' },
+        { main: '#5D8F79' },
+        { main: '#FAD3D0' },
+        // secondary
+        { main: '#5F696E' },
+        { main: '#8796BE' },
+        { main: '#66A8C3' },
+        { main: '#8796BE' },
+        { main: '#CBABAE' },
+        { main: '#9A9985' },
+        { main: '#BF7474' },
+      ],
+      primaryBlue: '#566C80',
+      lightBlue: 'rgba(63, 81, 181, 0.08)',
+      lightTitle: 'rgba(0, 0, 0, 0.87)',
+      lightMessage: '#b0bec5',
+      // leaving this here in case the color theme changes base on dark mode
+      extLink: '#66A8C3',
+    },
+  })
+}
+
 export function createTheme(options: SimpleThemeOptions): CCFTheme {
   const themeOptions = createThemeOptions(options)
   const baseTheme = createMuiTheme(themeOptions) as CCFTheme
